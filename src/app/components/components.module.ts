470d130723c4ddacbc57547c3e0f96af5e2961b1
Christian Salto Perez <christiansaltoperez@gmail.com>
2024-04-11 12:14:31 +0200

    Graficas

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { ChartsModule } from 'ng2-charts';
 
 import { IncreaserComponent } from './increaser/increaser.component';
+import { DonaComponent } from './dona/dona.component';
 
 
 @NgModule({
-  declarations: [IncreaserComponent],
-  exports: [IncreaserComponent],
-  imports: [CommonModule, FormsModule],
+  declarations: [IncreaserComponent, DonaComponent],
+  exports: [IncreaserComponent, DonaComponent],
+  imports: [CommonModule, FormsModule, ChartsModule],
 })
 export class ComponentsModule {}
